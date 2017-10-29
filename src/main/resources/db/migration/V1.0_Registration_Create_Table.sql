6178d1e8df0679cebdf2e5daf9d832102638630c
kkandukuri <kkandukuri@shutterfly.com>
2017-10-29 09:16:28 -0700

    DB CHanges

--- src/main/resources/db/migration/V1.0_Registration_Create_Table.sql
+++ src/main/resources/db/migration/V1.0_Registration_Create_Table.sql
@@ -5,7 +5,7 @@ CREATE TABLE `classroom`.`registration` (
   `password` VARCHAR(48) NULL,`created_date` DATETIME NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=latin1;
   
-  CREATE TABLE `classroom`.`academicwiki` (
+  CREATE TABLE `digimags`.`academicwiki` (
   `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
     `articlename` VARCHAR(24) NULL DEFAULT NULL, `studentname` VARCHAR(24) NULL DEFAULT NULL,
   `pdfname` VARCHAR(48) NULL DEFAULT NULL,`link` VARCHAR(48) NULL DEFAULT NULL,`resource` VARCHAR(48) NULL DEFAULT NULL,
